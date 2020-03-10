7396f6424a01da7ea4850aa815259ee500ddf742
camoal <55301053+camoal@users.noreply.github.com>
2020-03-10 22:08:55 +0100

    Internal extensions

--- src/main/java/com/camoal/jwe/Extensions.kt
+++ src/main/java/com/camoal/jwe/Extensions.kt
@@ -3,17 +3,17 @@ package com.camoal.jwe
 import android.util.Base64
 import java.security.MessageDigest
 
-fun ByteArray.toSha256(): ByteArray {
+internal fun ByteArray.toSha256(): ByteArray {
     val messageDigest = MessageDigest.getInstance("SHA-256")
     return messageDigest.digest(this)
 }
 
-fun ByteArray.toBase64(): String {
+internal fun ByteArray.toBase64(): String {
     return Base64.encodeToString(this,
         Base64.URL_SAFE or Base64.NO_PADDING or Base64.NO_WRAP)
 }
 
-fun String.fromBase64(): ByteArray {
+internal fun String.fromBase64(): ByteArray {
     return Base64.decode(this,
         Base64.URL_SAFE or Base64.NO_PADDING or Base64.NO_WRAP)
 }
